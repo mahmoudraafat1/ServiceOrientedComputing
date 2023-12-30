c9a0f3dfdcd33940663525baa8c3c252301b4ed6
Mahmoud Raafat <69508027+mahmoudraafat1@users.noreply.github.com>
2023-12-30 18:17:50 +0200

    Update cart.controller.ts

--- services/library-microservice/src/cart/cart.controller.ts
+++ services/library-microservice/src/cart/cart.controller.ts
@@ -1,14 +1,42 @@
-import { Controller } from '@nestjs/common';
-import { MessagePattern } from '@nestjs/microservices';
+/* eslint-disable prettier/prettier */
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Body,
+  Param,
+} from '@nestjs/common';
 import { CartService } from './cart.service';
+import { CreateCartDto, UpdateCartDto } from './cart.dto';
 
-@Controller()
+@Controller('carts')
 export class CartController {
   constructor(private readonly cartService: CartService) {}
 
-  @MessagePattern({ cmd: 'add-to-cart' })
-  async addToCart(data: { userId: string; bookId: string }): Promise<void> {
-    const { userId, bookId } = data;
-    await this.cartService.addToCart(userId, bookId);
+  @Get()
+  findAll() {
+    return this.cartService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.cartService.findOne(id);
+  }
+
+  @Post()
+  create(@Body() createCartDto: CreateCartDto) {
+    return this.cartService.create(createCartDto);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() updateCartDto: UpdateCartDto) {
+    return this.cartService.update(id, updateCartDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.cartService.remove(id);
   }
 }
