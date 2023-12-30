230313da2abf89a30c0ac3e767a21c9537537b50
Mahmoud Raafat <69508027+mahmoudraafat1@users.noreply.github.com>
2023-12-30 18:41:50 +0200

    Update cart.service.ts

--- services/library-microservice/src/cart/cart.service.ts
+++ services/library-microservice/src/cart/cart.service.ts
@@ -1,14 +1,39 @@
+/* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Cart, Item } from './cart.interface';
+import { CreateCartDto, UpdateCartDto } from './cart.dto';
 
 @Injectable()
 export class CartService {
-  private cartItems: Map<string, string[]> = new Map();
+  constructor(
+    @InjectModel('Cart') private readonly cartModel: Model<Cart>,
+  ) {}
 
-  async addToCart(userId: string, bookId: string): Promise<void> {
-    if (!this.cartItems.has(userId)) {
-      this.cartItems.set(userId, []);
-    }
-    const userCart = this.cartItems.get(userId);
-    userCart.push(bookId);
+  async findAll(): Promise<Cart[]> {
+    return this.cartModel.find().exec();
+  }
+
+  async findOne(id: string): Promise<Cart> {
+    return this.cartModel.findById(id).exec();
+  }
+
+  async create(createCartDto: CreateCartDto): Promise<Cart> {
+    const createdCart = new this.cartModel(createCartDto);
+    return createdCart.save();
+  }
+
+  async update(
+    id: string,
+    updateCartDto: UpdateCartDto,
+  ): Promise<Cart> {
+    return this.cartModel.findByIdAndUpdate(id, updateCartDto, {
+      new: true,
+    });
+  }
+
+  async remove(id: string): Promise<Cart> {
+    return this.cartModel.findOneAndDelete({ _id: id });
   }
 }
