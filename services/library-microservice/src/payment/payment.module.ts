ae3a694fea9e1ad8ca10de2e5b43e2a16e149830
ranahaytham <115706883+ranahaytham@users.noreply.github.com>
2024-01-04 15:54:01 -0800

    Update payment.module.ts

--- services/library-microservice/src/payment/payment.module.ts
+++ services/library-microservice/src/payment/payment.module.ts
@@ -1,4 +1,17 @@
 import { Module } from '@nestjs/common';
+import { PaymentController } from './payment.controller';
+import { PaymentService } from './payment.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { PaymentSchema, } from './payment.model'; // Import the schema
 
-@Module({})
-export class PaymentModule {}
+
+
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: 'payment', schema: PaymentSchema }]), // Provide ItemModel
+  ],
+  controllers: [PaymentController],
+  providers: [PaymentService]
+})
+export class paymentModule {}
