1be4f9a8b389e8f5b186116d9f31862215f68345
ranahaytham <115706883+ranahaytham@users.noreply.github.com>
2024-01-04 15:53:23 -0800

    Update payment.service.ts

--- services/library-microservice/src/payment/payment.service.ts
+++ services/library-microservice/src/payment/payment.service.ts
@@ -1,4 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable,Inject } from '@nestjs/common';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+import {  Payment} from './payment.model';
+
 
 @Injectable()
-export class PaymentService {}
+export class PaymentService {
+    constructor(@Inject('paymentModel') private readonly paymentModel: Model<Payment>) {}
+
+  async create(user: Payment): Promise<Payment> {
+    const newUser = new this.paymentModel(user);
+    return await newUser.save();
+  }
+
+  async findAll(): Promise<Payment[]> {
+    return await this.paymentModel.find().exec();
+  }
+
+  async findOne(id: string): Promise<Payment> {
+    return await this.paymentModel.findById(id).exec();
+  }
+
+  async update(id: string, Payment: Payment): Promise<Payment> {
+    return await this.paymentModel.findByIdAndUpdate(id, Payment, { new: true }).exec();
+  }
+
+  async delete(id: string): Promise<any> {
+    return await this.paymentModel.deleteOne({ _id: id }).exec();
+  }
+}
